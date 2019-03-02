6ce59a290110f9468cb27835e10d9a711e6cf00b
Tommo J. Phillips <tommojphillips@gmail.com>
2019-03-03 01:34:34 +1100

    Add project files.

---
+++ CruiseControlMod/CruiseControlPanel.cs
@@ -0,0 +1,69 @@
+﻿using ModApi.Attachable;
+using UnityEngine;
+
+namespace CruiseControlMod
+{
+    internal class CruiseControlPanel : Part
+    {
+        // Written, 18.10.2018
+
+        #region Fields
+
+        /// <summary>
+        /// Represents the scale of the cruise control unit.
+        /// </summary>
+        internal static readonly Vector3 scale = new Vector3(100, 100, 100);
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Represents the default part info.
+        /// </summary>
+        protected override PartSaveInfo defaultPartSaveInfo => new PartSaveInfo()
+        {
+            installed = false, // starts installed?
+            position = new Vector3(-9.212333f, 0.08187382f, 9.214458f), // Next to phone
+            rotation = Quaternion.Euler(270, 126.112f, 0)
+        };
+        /// <summary>
+        /// Represents the rigid part; the installed part.
+        /// </summary>
+        protected override GameObject rigidPart
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Represents the active part; the free/pickable part.
+        /// </summary>
+        protected override GameObject activePart
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Gets save info from encapsulated method.
+        /// </summary>
+        internal PartSaveInfo getPartSaveInfo
+        {
+            get
+            {
+                return this.getSaveInfo();
+            }
+        }
+
+        #endregion
+
+        public CruiseControlPanel(PartSaveInfo inPartSaveInfo, GameObject part, GameObject parent, Trigger inPartTrigger, Vector3 inPartPosition, Quaternion inPartRotation) : base(inPartSaveInfo, part, parent, inPartTrigger, inPartPosition, inPartRotation)
+        {
+            // Written, 18.10.2018
+
+            // Adding cruise control function to the rigid part, (installed).
+            this.rigidPart.AddComponent<CruiseControl>();
+            // Adding fix scale function to parts.
+            this.activePart.AddComponent<FixedScaleMono>();
+        }
+    }
+}
---
+++ CruiseControlMod/FixedScaleMono.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace CruiseControlMod
+{
+    /// <summary>
+    /// Represents the active cruise control panel part.
+    /// </summary>
+    internal class FixedScaleMono : MonoBehaviour
+    {
+        // Written, 18.10.2018
+
+        /// <summary>
+        /// Occurs every frame.
+        /// </summary>
+        private void Update()
+        {
+            // Written, 18.10.2018
+
+            this.fixScale();
+        }
+        /// <summary>
+        /// Assigns the gameobjects scale. Fixes issue when picked object up and put down resets scale to 1,1,1.
+        /// </summary>
+        private void fixScale()
+        {
+            // Written, 27.09.2018
+
+            if (this.gameObject.transform.hasChanged)
+            {
+                this.gameObject.transform.localScale = CruiseControlPanel.scale; // For some reason when picked up ccpanel and then letting go resets the scale to 1,1,1. preventing that..
+                this.gameObject.transform.hasChanged = false;
+            }
+        }
+    }
+}
