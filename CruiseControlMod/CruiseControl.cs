06b857d2bed74b5bfcf1f395f6acd07ace02c8d1
Tommo J. Phillips <tommojphillips@gmail.com>
2019-03-03 01:34:34 +1100

    Add project files.

---
+++ CruiseControlMod/CruiseControl.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using UnityEngine;
+using MSCLoader;
+using ModApi;
+
+namespace CruiseControlMod
+{
+    /// <summary>
+    /// Represents Cruise Control Function for the Cruise Control Unit.
+    /// </summary>
+    internal class CruiseControl : MonoBehaviour
+    {
+        // Written, 27.09.2018
+
+        #region Properties
+
+        // Instance Properties
+        /// <summary>
+        /// Represents whether the stock steering wheel is installed.
+        /// </summary>
+        private bool isStockSteeringWheelInstalled => (this.gameObject // ccPanel that's attached to the stock steering wheel.
+                                                        .transform?.parent? // stock steering wheel.
+                                                        .transform?.parent? // stock steering wheel's parent (could be null so '?' null operator use).
+                                                        .name == "pivot_stock_wheel"); // Either the name of the parent is equal to null or 'pivot_sto....' .           
+
+        // Static Properties
+        /// <summary>
+        /// Represents the audio for the dash buttons.
+        /// </summary>
+        private static AudioSource dashButtonAudioSource
+        {
+            get
+            {
+                return GameObject.Find("dash_button").GetComponent<AudioSource>();
+            }
+        }
+        /// <summary>
+        /// Represents whether the satsuma has power.
+        /// </summary>
+        private static bool hasPower
+        {
+            get
+            {
+                GameObject elect = GameObject.Find("SATSUMA(557kg, 248)/Electricity");
+                PlayMakerFSM power = PlayMakerFSM.FindFsmOnGameObject(elect, "Power");
+                return power.FsmVariables.FindFsmBool("ElectricsOK").Value;
+            }
+        }
+        /// <summary>
+        /// Represents the clear button of the ccUnit.
+        /// </summary>
+        private static GameObject clearButton
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Represents the power button of the ccUnit.
+        /// </summary>
+        private static GameObject powerButton
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Represents the res button of the ccUnit.
+        /// </summary>
+        private static GameObject resButton
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Represents the set button of the ccUnit.
+        /// </summary>
+        private static GameObject setButton
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Represents the wires of the ccUnit.
+        /// </summary>
+        private static GameObject wires
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets up the cruise control unit.
+        /// </summary>
+        private void Start()
+        {
+            // Written, 27.09.2018
+
+            // Setting Clear Button
+            clearButton = this.gameObject.transform.FindChild("BtnClr").gameObject;
+            clearButton.AddComponent<MeshCollider>().convex = true;
+            clearButton.name = "Clear";
+            // Setting power Button
+            powerButton = this.gameObject.transform.FindChild("BtnOnOff").gameObject;
+            powerButton.AddComponent<MeshCollider>().convex = true;
+            powerButton.name = "Power";
+            // Setting Res Button
+            resButton = this.gameObject.transform.FindChild("BtnRes").gameObject;
+            resButton.AddComponent<MeshCollider>().convex = true;
+            resButton.name = "Res";
+            // Setting Set Button
+            setButton = this.gameObject.transform.FindChild("BtnSet").gameObject;
+            setButton.AddComponent<MeshCollider>().convex = true;
+            setButton.name = "Set";
+            // Setting Wires
+            wires = this.gameObject.transform.FindChild("Wires").gameObject;
+        }
+        /// <summary>
+        /// Runs every frame.
+        /// </summary>
+        private void Update()
+        {
+            // Written, 27.09.2018
+
+            this.ccFunction();
+        }
+
+        /// <summary>
+        /// The function of the cruise control unit.
+        /// </summary>
+        private void ccFunction()
+        {
+            // Written, 27.09.2018
+
+            if (this.isStockSteeringWheelInstalled)
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1f, 1 << this.gameObject.layer))
+                {
+                    bool foundObject = false;
+                    bool foundParent = false;
+
+                    if (hit.transform?.gameObject?.name == "SATSUMA(557kg, 248)")
+                    {
+                        foundParent = true;
+                        GameObject gameObjectHit;
+
+                        gameObjectHit = hit.collider?.gameObject;
+
+                        if (gameObjectHit != null)
+                        {
+                            Action actionToPerform = null;
+
+                            if (gameObjectHit == clearButton)
+                            {
+                                foundObject = true;
+                                actionToPerform = this.onClear;
+                            }
+                            else
+                            {
+                                if (gameObjectHit == powerButton)
+                                {
+                                    foundObject = true;
+                                    actionToPerform = this.onPower;
+                                }
+                                else
+                                {
+                                    if (gameObjectHit == resButton)
+                                    {
+                                        foundObject = true;
+                                        actionToPerform = this.onRes;
+                                    }
+                                    else
+                                    {
+                                        if (gameObjectHit == setButton)
+                                        {
+                                            foundObject = true;
+                                            actionToPerform = this.onSet;
+                                        }
+                                    }
+                                }
+                            }
+                            if (foundObject)
+                            {
+                                ModClient.guiInteract(gameObjectHit.name);
+                                if (CruiseControlMod.useButtonDown)
+                                {
+                                    if (hasPower)
+                                        actionToPerform.Invoke();
+                                    AudioSource audio = dashButtonAudioSource;
+                                    audio.transform.position = gameObjectHit.transform.position;
+                                    audio.Play();
+                                }
+                            }
+                        }
+                    }
+                    if (foundParent && !foundObject) // Only reset if gui use was active.
+                    {
+                        ModClient.guiInteract();
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Occurs when the clear button is pressed.
+        /// </summary>
+        private void onClear()
+        {
+            ModConsole.Print("Clear button clicked");
+        }
+        /// <summary>
+        /// Occurs when the power button is pressed.
+        /// </summary>
+        private void onPower()
+        {
+            ModConsole.Print("Power button clicked");
+        }
+        /// <summary>
+        /// Occurs when the res button is pressed.
+        /// </summary>
+        private void onRes()
+        {
+            ModConsole.Print("Res button clicked");
+        }
+        /// <summary>
+        /// Occurs when the set button is pressed.
+        /// </summary>
+        private void onSet()
+        {
+            ModConsole.Print("Set button clicked");
+        }        
+
+        #endregion
+    }
+}
---
+++ CruiseControlMod/CruiseControlMod.cs
@@ -0,0 +1,110 @@
+﻿using MSCLoader;
+using UnityEngine;
+using ModApi.Attachable;
+
+namespace CruiseControlMod
+{
+    //CruiseControl Mod Class
+    //Remember to check the DLL references:
+    // cInput.dll
+    // modapi_v0114-alpha.dll
+    // MSCLoader.dll
+    // PlayMaker.dll
+    // UnityEngine.dll
+    public class CruiseControlMod : Mod
+    {
+        public override string ID { get { return "CruiseControl"; } }
+        public override string Name { get { return "Satsuma Cruise Control"; } }
+        public override string Author { get { return "Nitro Pascal"; } }
+        public override string Version { get { return "0.11"; } }
+        public override bool UseAssetsFolder => true;
+
+        /// <summary>
+        /// Represents the file name for the save file.
+        /// </summary>
+        private const string fileName = "SatsumaCCSaveData.txt";
+        /// <summary>
+        /// Represents the Cruise Control 3d model as a gameobject.
+        /// </summary>
+        private GameObject ccPanel;
+        /// <summary>
+        /// Represents the cruise control panel game object.
+        /// </summary>
+        private CruiseControlPanel cruiseControlPanel;
+        /// <summary>
+        /// Represents whether the use button is being held down.
+        /// </summary>
+        public static bool useButtonDown
+        {
+            get
+            {
+                return cInput.GetKeyDown("Use");
+            }
+        }
+
+        /// <summary>
+        /// Loads save data.
+        /// </summary>
+        /// <returns></returns>
+        private PartSaveInfo loadSaveData()
+        {
+            // Written, 12.10.2018
+
+            try
+            {
+                return SaveLoad.DeserializeSaveFile<PartSaveInfo>(this, fileName);
+            }
+            catch (System.NullReferenceException)
+            {
+                // no save file exists.. //loading default save data.
+
+                return null;
+            }
+        }
+        //Called when mod is loading
+        public override void OnLoad()
+        {
+            try
+            {                
+                // Creating "ccpanel"
+                GameObject temp_ccPanel;
+                AssetBundle bundle = LoadAssets.LoadBundle(this, "cruiseassets.unity3d");
+                temp_ccPanel = bundle.LoadAsset("ccobject.prefab") as GameObject;
+                // Making panel owner of buttons and wires.
+                this.ccPanel = temp_ccPanel.transform.FindChild("Panel").gameObject;
+                temp_ccPanel.transform.FindChild("BtnClr").gameObject.transform.SetParent(this.ccPanel.transform);
+                temp_ccPanel.transform.FindChild("BtnOnOff").gameObject.transform.SetParent(this.ccPanel.transform);
+                temp_ccPanel.transform.FindChild("BtnRes").gameObject.transform.SetParent(this.ccPanel.transform);
+                temp_ccPanel.transform.FindChild("BtnSet").gameObject.transform.SetParent(this.ccPanel.transform);
+                temp_ccPanel.transform.FindChild("Wires").gameObject.transform.SetParent(this.ccPanel.transform);
+                // Setting scale of panel.
+                this.ccPanel.transform.localScale = CruiseControlPanel.scale;
+                // Adding rigidbody and mesh collider.
+                this.ccPanel.AddComponent<Rigidbody>();
+                this.ccPanel.AddComponent<MeshCollider>().convex = true;
+                // loading part data
+                PartSaveInfo ccPanel_partSaveInfo = this.loadSaveData();
+                // Creating ccpanel part.
+                this.ccPanel.name = "Cruise Control Panel";
+                GameObject _parent = GameObject.Find("stock steering wheel(Clone)");
+                Trigger trigger = new Trigger("ccPanelTrigger", _parent, new Vector3(0.112f, 0.042f, 0.016f), Quaternion.Euler(0, 0, 80.00001f), new Vector3(0.1f, 0.1f, 0.1f), true);
+                this.cruiseControlPanel = new CruiseControlPanel(ccPanel_partSaveInfo, ccPanel, _parent, trigger, new Vector3(0.112f, 0.042f, 0.016f), Quaternion.Euler(0, 0, 80.00001f));
+                // Destorying ccpanel copy.
+                Object.Destroy(this.ccPanel);
+                // Unloading bundle.
+                bundle.Unload(false);
+                ModConsole.Print(string.Format("{0}, v{1}: Loaded", this.Name, this.Version));
+
+            }
+            catch (System.Exception ex)
+            {
+                ModConsole.Error(string.Format("Error: {0}\r\nStacktrace: {1}", ex.Source, ex.StackTrace));
+            }
+        }
+        //Called when player goes to the toilet
+        public override void OnSave()
+        {
+            SaveLoad.SerializeSaveFile(this, this.cruiseControlPanel.getPartSaveInfo, fileName);
+        }
+    }
+}
